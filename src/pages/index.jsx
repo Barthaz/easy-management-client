c8c46857f9291d8e1b0b77d181b27766117eb589
BK <bartosz.kuligowski@e-file.pl>
2022-10-27 20:53:08 +0200

    project setup + sidebar

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as Analytics } from './Analytics';
+export { default as Calendar } from './Calendar';
+export { default as Home } from './Home';
+export { default as Kanban } from './Kanban';
+export { default as Logs } from './Logs';
+export { default as Team } from './Team';
+export { default as Register } from './Register';
+export { default as Login } from './Login';
